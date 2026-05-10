4e568a4ebbd272e91e58ae899d21486711195b1c
alamgir <alamgir.bfew@gmail.com>
2026-05-11 02:23:02 +0600

    swagger

--- NZHRM/NZ.HRM.WebAPI/Program.cs
+++ NZHRM/NZ.HRM.WebAPI/Program.cs
@@ -40,11 +40,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 app.UseCors("AllowAll");
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
