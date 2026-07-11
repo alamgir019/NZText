88012e60b10ce8133e13ddb9a56c43fe0452828e
alamgir <alamgir.bfew@gmail.com>
2026-07-11 10:41:53 +0600

    cancel ids

--- NZHRM/NZ.HRM.Application.Model/Employees/Commands/CreateCompleteEmployee/CreateCandidateEntryCommand.cs
+++ NZHRM/NZ.HRM.Application.Model/Employees/Commands/CreateCompleteEmployee/CreateCandidateEntryCommand.cs
@@ -17,16 +17,7 @@ public class CreateCandidateEntryCommand
     public string UnitId { get; set; } = string.Empty;
 
     //[Required(ErrorMessage = "Department ID is required")]
-    public string? DepartmentId { get; set; } = string.Empty;
     public string? DesignationId { get; set; }
-
-    //[Required(ErrorMessage = "Location ID is required")]
-    public string? LocationId { get; set; } = string.Empty;
-
-    //[Required(ErrorMessage = "Section ID is required")]
-    public string? SectionId { get; set; } = string.Empty;
-
-    public string? CellId { get; set; }
 
     public decimal? ProposedMonthlySalary { get; set; }
 
